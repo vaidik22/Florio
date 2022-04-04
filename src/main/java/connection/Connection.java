495649ce7fb68d4d31c050bf5b533338712cc856
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2022-04-05 01:20:01 +0530

    connection to database

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -6,6 +6,32 @@
 
 package connection;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+    private static final String URL = "jdbc:mysql://localhost:3306/florio";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static java.sql.Connection connection = null;
+
+    private static java.sql.Connection getConnection() {
+        return connection;
+    }
+
+    public static boolean startConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return connection != null;
+    }
+
+    public static void printStatus() {
+        System.out.println(connection != null ? "ONLINE" : "OFFLINE");
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.github.prajjwal.florio {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens io.github.prajjwal.florio to javafx.fxml;
